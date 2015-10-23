fe723a1a82f2094ff216b292d4cdea73381eaae1
Hadiya Hope-Crichlow <hopecrichlow@live.com>
2015-10-23 14:56:58 -0400

    Initial Commit

---
+++ js/tests.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  describe('About our pond', function() {
+
+    describe('creating a new Pond', function() {
+
+      let p = Pond;
+      it('should run here few assertions', function() {
+        expect(1).to.equal(1);
+      });
+    });
+  });
+
+}());
